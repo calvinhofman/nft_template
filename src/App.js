f516535faa33c5342f0be0e0468d6c0af335555d
calvinhofman <60505346+calvinhofman@users.noreply.github.com>
2023-06-02 13:58:12 +0200

    last commit tried reworking so i dont use thirdweb

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 // Web3 stuff
 
 // images
-import headerlogo from "./images/HeaderLogo.png";
-import steakdm from "./images/steakd.png";
-import incredibulls from "./images/INCREDIBULLS.png";
 import mystery from "./images/mystery.png";
-import tempimage from "./images/Home_3.png";
-import {
-  ConnectWallet,
-  useAddress,
-  useContractRead,
-  useContract,
-  useContractWrite,
-  Web3Button,
-} from "@thirdweb-dev/react";
+import Web3 from "web3";
+// import WalletConnectProvider from "@walletconnect/web3-provider";
+
+
+
+import abi from './ABI/nft-abi.json';
+import { useEffect } from "react";
+
 
 // styling
 import "./styles/Home.css";
@@ -21,34 +17,46 @@ import { useState } from "react";
 import { utils } from "ethers";
 
 export default function Home() {
-  let nftCount;
   const contractAddress = "0x6489265Bf18185cA693017E7448bD77E8F80A524";
-  const address = useAddress();
   const [quantity, setQuantity] = useState(0);
-  const { contract } = useContract(contractAddress);
   const [showModal, setShowModal] = useState(false);
+  const [showModalConnect, setShowModalConnect] = useState(false);
+  const [web3, setWeb3] = useState(null);
+  const [connectedAddress, setConnectedAddress] = useState(null);
+
+
+
+
   const [showModalonce, setShowModalonce] = useState(true);
   const [nftAmount, setNftAmount] = useState(0);
+  let [price, setPrice] = useState(0);
+  let [count, setCount] = useState(0);
+  let [data, setData] = useState(0);
 
-  const { data: data, isLoading } = useContractRead(contract, "maxTokenID");
-  let { data: price, isLoading1 } = useContractRead(contract, "mintPrice");
-  const { data: count, isLoading4 } = useContractRead(contract, "nftCount");
-  console.log(count?.toString());
+
+
+
+  // const contract = new web3.eth.Contract(abi, contractAddress);
+
+  async function fetchData() {
+    // let price = await contract.methods.mintPrice().call();
+    // let count = await contract.methods.nftCount().call();
+    // let data = await contract.methods.maxTokenID().call();
+
+    setPrice(price);
+    setCount(count);
+    setData(data);
+  }
 
   price = price * quantity;
 
-  const { mutateAsync: mintNFT, isLoading2 } = useContractWrite(
-    contract,
-    "mintNFT"
-  );
-
   const call = async () => {
-    try {
-      // const data = await mintNFT({ args: [ address, quantity] }, {value: ( price?.toString() )});
-      const data = await contract.call("mintNFT", [address, quantity], {
-        value: price?.toString(),
-      });
 
+
+
+
+    try {
+      // const data = await contract.methods.mintNFT(connectedAddress, quantity).send({ from: connectedAddress });
       let nftCount = +count?.toString() + +quantity - 1;
       setNftAmount(nftCount);
       setShowModal(true);
@@ -70,58 +78,126 @@ export default function Home() {
     setShowModal(false);
   };
 
+  const handleCloseModalconnect = () => {
+    setShowModalConnect(false);
+  };
+
+  const handleOpenModalconnect = () => {
+    setShowModalConnect(true);
+  };
+
+
+    // Function to handle wallet connect button click
+    const handleWalletConnect = () => {
+      // const provider = new WalletConnectProvider({
+      //   rpc: {
+      //     56: 'https://bsc-dataseed1.binance.org/',
+      //   },
+      // });
+      // provider
+      //   .enable()
+      //   .then(() => {
+      //     const web3 = new Web3(provider);
+      //     setWeb3(web3);
+
+      //     web3.eth.getAccounts().then((accounts) => {
+      //       const address = accounts[0];
+      //       setConnectedAddress(address);
+      //       handleCloseModalconnect();
+      //       // Additional logic after successful connection
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error connecting with WalletConnect:', error);
+      //   });
+    };
+
+    // Function to handle MetaMask button click
+    const handleMetaMaskConnect  = async () => {
+      if (window.ethereum) {
+        try {
+          await window.ethereum.request({ method: "eth_requestAccounts" });
+          const web3 = new Web3(window.ethereum);
+          setWeb3(web3);
+
+          const accounts = await web3.eth.getAccounts();
+          const address = accounts[0];
+          setConnectedAddress(address);
+          handleCloseModalconnect()
+          // Additional logic after successful connection
+        } catch (error) {
+          console.error("Error connecting to Metamask:", error);
+        }
+      } else {
+        console.error("Metamask not found");
+      }
+    }
+
+    const handleLogout = () => {
+      setConnectedAddress(null);
+      // Additional logic for logging out
+    };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="">
-      <div className="flex flex-col sm:flex-row justify-between text-black items-center w-8/12 mx-auto mt-12">
-        <div>
-          <p className="font-bold sm:text-3xl text-xl ">New NFT collection</p>
-        </div>
-        <div>
-          <img className="invisible md:visible" src={headerlogo} alt="" />
-        </div>
-        <div>
-          <ConnectWallet theme="white" />
+<div className="flex flex-col sm:flex-row justify-between text-black items-center w-8/12 mx-auto mt-12">
+        <div className="mx-auto flex items-center">
+
+          {connectedAddress ? (
+            <div className="flex items-center bg-[#0ea5e9] rounded-lg px-4 py-2">
+                          <img
+              src="https://ipfs.thirdwebcdn.com/ipfs/QmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9/binance-coin/512.png"
+              alt="Binance Smart Chain"
+              className="w-6 h-6 mr-2"
+            />
+              <p
+                className="text-gray-600 cursor-pointer"
+                onClick={handleLogout}
+              >
+                {connectedAddress.substring(0, 6)}...
+                {connectedAddress.substring(connectedAddress.length - 4)}
+              </p>
+              {connectedAddress && (
+                <img
+                  src="https://ipfs.thirdwebcdn.com/ipfs/QmZZHcw7zcXursywnLDAyY6Hfxzqop5GKgwoq8NB9jjrkN/metamask.svg"
+                  alt="Metamask"
+                  className="w-6 h-6 ml-2"
+                />
+              )}
+            </div>
+          ) : (
+            <button
+              onClick={handleOpenModalconnect}
+              className="bg-[#0ea5e9] rounded-lg px-4 py-2 text-white font-semibold"
+            >
+              Connect wallet
+            </button>
+          )}
         </div>
       </div>
-      <div className="flex flex-col px-12">
-        <img className="mx-auto w-8/12" src={steakdm} alt="" />
-        <img className="mx-auto mt-12  w-7/12 " src={incredibulls} alt="" />
-      </div>
-      <div className="flex flex-row flex-wrap justify-between text-black w-8/12 mx-auto h-40 mt-12">
-        <div className="flex flex-col justify-between font-bold text-2xl">
-          <div>
-            {" "}
-            {count?.toString()
-              ? `${
-                count?.toString() === "0"
-                    ? "0"
-                    : count?.toString() - 1
-                }`
-              : "0 "}
-             / {data?.toString()
-              ? `${
-                data?.toString() === "0"
-                    ? "0"
-                    : data?.toString()
-                }`
-              : "0"}  Minted
-          </div>
-          <div>
+
+      <div className="flex  flex-col flex-wrap justify-between text-black w-8/12 mx-auto mt-12">
+        <div className="flex flex-col justify-start font-bold text-2xl mx-auto">
+
+          <div className="mt-12">
             Quantity{" "}
             <input
-              className="w-12 ml-8"
+              className="w-12 text-center border-2 border-gray-400"
               type="number"
               value={quantity}
               onChange={(e) => setQuantity(parseInt(e.target.value))}
             />
           </div>
         </div>
-        <div className="flex flex-col justify-between font-bold text-xl lg:text-2xl">
-          <div>Steakd presents the Incredibulls!</div>
+        <div className="flex flex-col justify-between font-bold text-xl lg:text-2xl mx-auto">
           <div>
             <div>
               <button onClick={handleClick} className="card">
-                MINT{" "}
+                MINT NFT{" "}
                 {price
                   ? `(${
                       price?.toString() === "0"
@@ -140,17 +216,28 @@ export default function Home() {
     >
       mintNFT
     </Web3Button> */}
+    <div className="text-center text-2xl mt-8">INCREDIBULLS MINTED</div>
+              <div className="text-center text-[#fd5a08]">
+            {" "}
+            {count?.toString()
+              ? `${
+                count?.toString() === "0"
+                    ? "0"
+                    : count?.toString() - 1
+                }`
+              : "0 "}
+             / {data?.toString()
+              ? `${
+                data?.toString() === "0"
+                    ? "0"
+                    : data?.toString()
+                }`
+              : "0"}
+          </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between font-bold text-2xl xl:w-3/12 w-10/12 lg:w-6/12 sm:w-10/12 md:mt-12 lg:text-xl lg:mt-12">
-          <p>
-            A zany and fun herd of cute and utility packed bulls ready to tear
-            up the Binance Smart Chain. Steakd Community Fam, SDX Staking Bonus,
-            liquidity protocols, and passive income!. Time to{" "}
-            <span className="text-[#FF742E]">#getSteakd</span>
-          </p>
-        </div>
+
       </div>
       {showModal && (
         <div>
@@ -163,10 +250,10 @@ export default function Home() {
                 </div>
                 <div className="sm:px-20">
                   <div className="">
-                    {" "}
-                    <h5 className="sm:text-7xl text-2xl text-center text-[#FF742E] font-extrabold">
+
+                    <h5 className="sm:text-7xl text-2xl text-center text-[#fd5a08] font-extrabold">
                       INCREDIBULLS
-                    </h5>{" "}
+                    </h5>
                   </div>
                   <div className="flex flex-col justify-between mx-6 mt-6">
                     <div className="flex flex-row justify-between text-black font-bold sm:text-2xl">
@@ -190,23 +277,29 @@ export default function Home() {
           </div>
         </div>
       )}
-
-      {showModalonce && (
+            {showModalConnect && (
         <div>
           <div className="w-screen h-screen bg-black absolute top-0 opacity-60 "></div>
           <div className="absolute w-screen h-screen top-0">
             <div class="grid place-items-center h-screen mx-auto">
-              <div className="p-4  bg-white opacity-100 rounded-xl">
-                <div className="text-black font-bold text-2xl flex justify-end">
-                  <button onClick={handleCloseModalonce}>X</button>
+              <div className="p-4  bg-[#161618] opacity-100 rounded-xl w-6/12">
+                <div className="text-white font-semibold text-2xl flex justify-between">
+                  <p>choose your wallet</p>
+                  <button onClick={handleCloseModalconnect}>X</button>
                 </div>
-
-                <img src={tempimage} alt="" />
+                <div className="sm:px-4">
+                  <div className="flex flex-col space-y-4 mt-4">
+                    <button id="metamask" onClick={handleMetaMaskConnect} className="bg-[#232326] px-4 py-2 rounded-lg flex flex-row justify-start items-center space-x-4"><img src="https://ipfs.thirdwebcdn.com/ipfs/QmZZHcw7zcXursywnLDAyY6Hfxzqop5GKgwoq8NB9jjrkN/metamask.svg" alt="" srcset="" /> <p>Metamask</p></button>
+                    <button id="walletconnect"   onClick={handleWalletConnect} className="bg-[#232326] px-4 py-2 rounded-lg flex flex-row justify-start items-center space-x-4"><img src="https://ipfs.thirdwebcdn.com/ipfs/QmX58KPRaTC9JYZ7KriuBzeoEaV2P9eZcA3qbFnTHZazKw/wallet-connect.svg" alt="" srcset="" /> <p>Metamask</p></button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       )}
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
       <App />
-    </ThirdwebProvider>
   </React.StrictMode>
 );
 
